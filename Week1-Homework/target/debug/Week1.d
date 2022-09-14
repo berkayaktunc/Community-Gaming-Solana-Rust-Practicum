f62f3d5320b83cd004e6ff902001c8308ba48cd2
berkayaktunc <berkayaktunc@gmail.com>
2022-09-14 13:42:09 +0300

    First week homework added

---
+++ Week1-Homework/target/debug/Week1.d
@@ -0,0 +1,1 @@
+/home/incodeable/Projects/__Community-Gaming-Solana&Rust-Practicum/Week1-Homework/target/debug/Week1: /home/incodeable/Projects/__Community-Gaming-Solana&Rust-Practicum/Week1-Homework/src/main.rs
---
+++ Week1-Homework/target/debug/deps/Week1-6e13f8316511d4ff.d
@@ -0,0 +1,5 @@
+/home/incodeable/Projects/__Community-Gaming-Solana&Rust-Practicum/Week1-Homework/target/debug/deps/Week1-6e13f8316511d4ff: src/main.rs
+
+/home/incodeable/Projects/__Community-Gaming-Solana&Rust-Practicum/Week1-Homework/target/debug/deps/Week1-6e13f8316511d4ff.d: src/main.rs
+
+src/main.rs:
